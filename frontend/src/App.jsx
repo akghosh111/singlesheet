e55ecfc8109b92f9650e3bfa067e8a2d2242d0e9
Siddhi Sahu <himanisahu739@gmail.com>
2025-03-16 21:36:27 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./Pages/Landing ";
+import LandingPage from "./Pages/Landing";
 import Navbar from "./components/Navbar";
 import { SignIn } from "./Pages/Signin";
+
 
 function App() {
   
